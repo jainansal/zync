292390bcdb6c56c575962b8497b2151678b690c9
Ansal Jain <jainansal@gmail.com>
2023-12-10 02:03:51 +0530

    ft: add command dialog

--- components/server/server-search.tsx
+++ components/server/server-search.tsx
@@ -1,4 +1,16 @@
-import { Command, Search } from "lucide-react";
+"use client";
+
+import { useEffect, useState } from "react";
+import { Search } from "lucide-react";
+
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
 
 interface ServerSearchProps {
   data: {
@@ -15,9 +27,25 @@ interface ServerSearchProps {
 }
 
 const ServerSearch = ({ data }: ServerSearchProps) => {
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        setOpen((open) => !open);
+      }
+    };
+    document.addEventListener("keydown", down);
+    return () => document.removeEventListener("keydown", down);
+  }, []);
+
   return (
     <>
-      <button className="group p-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition">
+      <button
+        className="group p-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition"
+        onClick={() => setOpen(true)}
+      >
         <Search className="w-4 h-4 text-zinc-500 dark:text-zinc-400" />
         <p className="font-semibold text-sm text-zinc-500 dark:text-zinc-400 group-hover:text-zinc-600 dark:group-hover:text-zinc-300 transition">
           Search
@@ -26,6 +54,28 @@ const ServerSearch = ({ data }: ServerSearchProps) => {
           Ctrl K
         </kbd>
       </button>
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandInput placeholder="Search all channels and members" />
+        <CommandList>
+          <CommandEmpty>No results found</CommandEmpty>
+          {data.map(({ label, data, type }) => {
+            if (!data?.length) return null;
+
+            return (
+              <CommandGroup key={label} heading={label}>
+                {data?.map(({ name, id, icon }) => {
+                  return (
+                    <CommandItem key={id} className="gap-x-2">
+                      {icon}
+                      <span>{name}</span>
+                    </CommandItem>
+                  );
+                })}
+              </CommandGroup>
+            );
+          })}
+        </CommandList>
+      </CommandDialog>
     </>
   );
 };
