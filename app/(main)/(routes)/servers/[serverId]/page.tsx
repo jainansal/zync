fd40f2c395c34c9a2111ad4bf30f1f1a110d35e2
Ansal Jain <jainansal@gmail.com>
2023-12-11 21:58:36 +0530

    fix: minor fixes

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,5 +1,45 @@
-const ServerIdPage = () => {
-  return <div>Server ID Page</div>;
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+
+interface ServerIdPageProps {
+  params: {
+    serverId: string;
+  };
+}
+
+const ServerIdPage = async ({ params }: ServerIdPageProps) => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  const server = await db.server.findUnique({
+    where: {
+      id: params.serverId,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+    include: {
+      channels: {
+        where: {
+          name: "general",
+        },
+      },
+    },
+  });
+
+  const generalChannel = server?.channels[0];
+
+  if (generalChannel?.name !== "general") return null;
+
+  return redirect(`/servers/${server?.id}/channels/${generalChannel?.id}`);
 };
 
 export default ServerIdPage;
